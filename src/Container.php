0ba83dc1152ae54bf4e92a51da11cfe1ea752b16
Dustin Simon <dustin.simon99@gmail.com>
2023-02-19 15:15:27 +0100

    Remove Encapsulation inheritance from Container

--- src/Container.php
+++ src/Container.php
@@ -2,33 +2,46 @@
 
 namespace Dustin\Encapsulation;
 
-class Container extends PropertyEncapsulation implements \Countable
+class Container implements \Countable, \IteratorAggregate, \JsonSerializable
 {
     private array $elements = [];
 
     public function __construct(array $elements = [])
     {
-        $this->setElements($elements);
+        foreach ($elements as $e) {
+            $this->add($e);
+        }
     }
 
-    public function getElements(): array
+    public function toArray(): array
     {
         return $this->elements;
     }
 
-    public function setElements(array $elements): void
+    public function add($element): void
     {
-        $this->elements = $elements;
+        $this->validateType($element);
+        $this->elements[] = $element;
     }
 
-    public function addElement($element): void
+    public function getAt(int $position)
     {
-        $this->elements[] = $element;
+        return array_values($this->elements)[$position] ?? null;
     }
 
     public function count(): int
     {
         return count($this->elements);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->elements);
+    }
+
+    public function clear(): void
+    {
+        $this->elements = [];
     }
 
     public function getIterator(): \Traversable
@@ -38,16 +51,36 @@ class Container extends PropertyEncapsulation implements \Countable
 
     public function jsonSerialize()
     {
-        return $this->elements;
+        return array_values($this->elements);
     }
 
     public function __serialize(): array
     {
-        return array_map('serialize', $this->elements);
+        return array_map('serialize', array_values($this->elements));
     }
 
     public function __unserialize(array $data): void
     {
-        $this->setElements(array_map('unserialize', $data));
+        $this->elements = array_values(array_map('unserialize', $data));
+    }
+
+    protected function getAllowedClass(): ?string
+    {
+        return null;
+    }
+
+    private function validateType($element)
+    {
+        $class = $this->getAllowedClass();
+
+        if ($class === null) {
+            return;
+        }
+
+        if (!is_object($element) || !($element instanceof $class)) {
+            $type = is_object($element) ? get_class($element) : gettype($element);
+
+            throw new \InvalidArgumentException(sprintf('Container can only hold %s got %s', $class, $type));
+        }
     }
 }
