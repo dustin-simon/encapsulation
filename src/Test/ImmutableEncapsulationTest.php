25a9e93b04a5f6ec7d155ca3feff7e9cf938ccb6
Dustin Simon <dustin.simon99@gmail.com>
2023-03-08 12:29:18 +0100

    Add unit test

--- src/Test/ImmutableEncapsulationTest.php
+++ src/Test/ImmutableEncapsulationTest.php
@@ -2,9 +2,11 @@
 
 namespace Dustin\Encapsulation\Test;
 
+use Dustin\Encapsulation\Encapsulation;
 use Dustin\Encapsulation\Exception\EncapsulationImmutableException;
 use Dustin\Encapsulation\ImmutableEncapsulation;
 use Dustin\Encapsulation\ImmutableTrait;
+use Dustin\Encapsulation\MutableInterface;
 use Dustin\Encapsulation\PropertyEncapsulation;
 use PHPUnit\Framework\TestCase;
 
@@ -31,5 +33,21 @@ class ImmutableEncapsulationTest extends TestCase
         $this->expectException(EncapsulationImmutableException::class);
 
         $encapsulation->set('foo', 'barBar');
+    }
+
+    public function testImmutableInterface()
+    {
+        $mutableEncapsulation = new Encapsulation();
+        $immutableEncapsulation1 = new ImmutableEncapsulation();
+        $immutableEncapsulation2 = new MyEncapsulation();
+
+        $this->assertTrue($mutableEncapsulation instanceof MutableInterface);
+        $this->assertTrue($mutableEncapsulation->isMutable());
+
+        $this->assertTrue($immutableEncapsulation1 instanceof MutableInterface);
+        $this->assertFalse($immutableEncapsulation1->isMutable());
+
+        $this->assertTrue($immutableEncapsulation2 instanceof MutableInterface);
+        $this->assertFalse($immutableEncapsulation2->isMutable());
     }
 }
